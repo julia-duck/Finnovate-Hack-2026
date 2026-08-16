1a24fb366a799123c2c85879bd897dcefa0ec868
Julia Jeng <juliajeng48@gmail.com>
2026-08-16 13:29:31 -0500

    Work on company.jsx

--- Finnovate-Hack-2026/src/App.jsx
+++ Finnovate-Hack-2026/src/App.jsx
@@ -1,24 +1,38 @@
 import { useState } from 'react'
 /*import reactLogo from './assets/react.svg'*/
 import './App.css'
+import {NavLink, Route, Routes} from 'react-router-dom'
+import Company from './Company.jsx'
 
 function App() {
 
   return (
     //html code here
     <div>
-      <Header />
-     <div style={{display: 'flex', justifyContent: 'center'}}>
-        <input class="search-bar" placeholder="Search Companies" />
-     </div>
+     <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/company" element={<Company/>} />
+      </Routes>
     </div>
   )
 }
 
-function Header() {
+export function Home() {
+  return ( 
+    <div>
+      <Header />
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <NavLink to="/company">Company</NavLink>
+        <input className="search-bar" placeholder="Search Companies" />
+      </div>
+    </div>
+  )
+}
+
+export function Header() {
   return (
     <div>
-      <h1 class="title">SUSTAINABUDDIES</h1>
+      <h1 className="title">SUSTAINABUDDIES</h1>
     </div>
   )
 }
